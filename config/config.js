1f44ec3441ce08d6db81697b1ef40c29d1707331
bunny vishal <bunnyvishal6@gmail.com>
2016-10-27 17:36:33 +0530

    addtion and deletion of stocks in realtime feature added

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    api_key: process.env.api_key,
+    api_key: process.env.api_key || "pLpP93hFEFfXpq8dzoip",
     api_version: 3,
-    db: process.env.db 
+    db: process.env.db || "mongodb://192.168.0.6:27017/stockchart"
 };
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 'use strict'
 let express = require('express'),
     app = express(),
+    server = require('http').Server(app),
+    io = require('socket.io')(server),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
     path = require('path'),
@@ -18,13 +20,115 @@ mongoose.connect(config.db);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//statis files serve 
+//static folder serve
+app.use(express.static(path.join(__dirname, 'public')));
+
+//static files serve 
 app.use("/public", express.static(path.join(__dirname, 'public')));
 
 //quandl init
 let quandl = new Quandl({
     auth_token: config.api_key,
     api_version: config.api_version
+});
+
+//on connection emit the intistocks
+io.on('connection', socket => {
+    console.log('new connection made');
+    //get all stocks and emit initstocks
+    Stock.find({}, (err, stocks) => {
+        if (err) { console.log(err); }
+        if (stocks.length === 0) {
+            socket.emit('initstocks', { stocks: [] });
+        } else {
+            let array = [];
+            for (let i = 0; i < stocks.length; i++) {
+                array.push(stocks[i].code);
+                if (i === stocks.length - 1) {
+                    socket.emit('initstocks', { stocks: array });
+                }
+            }
+        }
+    });
+
+    //on deletestock delete the stock from database and emit the stock to client so that deletion done in client as well
+    socket.on('deletestock', data => {
+        Stock.findOne({ code: data.stock.code }, (err, stock) => {
+            if (err) { console.log(err) }
+            if (stock) {
+                stock.remove(err => {
+                    if (!err) {
+                        io.emit('deletedstock', { stock: data.stock });
+                    }
+                });
+            }
+        })
+    });
+
+    //on addstock add the stock code to database and emit the stock code to client so that the addition is done in client as well.
+    socket.on('addstock', emitedData => {
+        let code = emitedData.code,
+            start_date = new Date(),
+            end_date = new Date();
+        end_date = end_date.getFullYear() + "-" + (end_date.getMonth() + 1) + "-" + end_date.getDate();
+        start_date = (start_date.getFullYear() - 1) + "-" + (start_date.getMonth() + 1) + "-" + start_date.getDate();
+        quandl.dataset(
+            {
+                source: 'WIKI',
+                table: code
+            },
+            {
+                order: 'asc',
+                exclude_column_names: false,
+                start_date: start_date,
+                end_date: end_date,
+                column_index: 1
+            },
+            (err, data) => {
+                if (err) {
+                    //emit addedstock error to particular client
+                     return socket.emit('addedstock', {err: err});
+                }
+                //if there is no quandl error then try to save the stock 
+                if (!JSON.parse(data).quandl_error) {
+                    Stock.findOne({ code: code }, (err, stock) => {
+                        if (err) { console.log(err); }
+                        if (!stock) {
+                            let newStock = new Stock({ code: code });
+                            newStock.save(err => {
+                                if (err) {
+                                    console.log(err);
+                                }
+                                //emitting addedstock for all clients
+                                io.emit('addedstock', {data: JSON.parse(data), code: code});
+                            });
+                        } else {
+                            //emitting addedstock for all clients
+                            io.emit('addedstock', {data: JSON.parse(data), code: code});
+                        }
+                    });
+                } else { // if quandl_error presents then emit the addedstock to particular client without attempting to save stock name.
+                    socket.emit('addedstock', {data: JSON.parse(data), code: code});
+                }
+            }
+        );
+    });
+
+
+});
+
+
+//delete a stock
+app.post('/api/deletestock', (req, res) => {
+    Stock.findOne({ code: req.body.stock.code }, (err, stock) => {
+        if (stock) {
+            stock.remove((err) => {
+                if (!err) {
+                    res.json("deleted successfully");
+                }
+            });
+        }
+    });
 });
 
 //home page
@@ -92,16 +196,6 @@ app.get('/api/stock/:code', (req, res) => {
     );
 });
 
-app.post('/api/deletestock', (req, res) => {
-    Stock.findOne({ code: req.body.stock.code }, (err, stock) => {
-        if (stock) {
-            stock.remove((err) => {
-                if (!err) {
-                    res.json("delete successfully");
-                }
-            });
-        }
-    });
-});
 
-app.listen(config.PORT || 3000);
+
+server.listen(config.PORT || 3000);
